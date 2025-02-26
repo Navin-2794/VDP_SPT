1f92422bfc403650d95e380af713b05bceccd5f3
mukeshdhoni6 <mukeshsubraj@gmail.com>
2025-02-26 13:28:29 +0530

    method added

--- src/test/java/com/FebIPT/AppTest.java
+++ src/test/java/com/FebIPT/AppTest.java
@@ -5,13 +5,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
-    
-  @Test
-  public void shouldAnswerWithTrue() {
-    assertTrue(true);
-  
-    System.out.println("Assertion is True...");
-    System.out.println("Team-C");
-  }
+
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
+
+		System.out.println("Assertion is True...");
+		System.out.println("Team-C");
+
+	}
+
+	public void m() {
+
+	}
 
 }

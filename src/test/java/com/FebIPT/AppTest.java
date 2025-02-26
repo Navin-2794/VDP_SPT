02424205985d9530903798b39716a84b9bbf94ff
vickyvk27 <vigneshkumar.vk902@gmail.com>
2025-02-26 09:06:25 +0530

    sysout added

--- src/test/java/com/FebIPT/AppTest.java
+++ src/test/java/com/FebIPT/AppTest.java
@@ -9,7 +9,9 @@ public class AppTest {
   @Test
   public void shouldAnswerWithTrue() {
     assertTrue(true);
+  
     System.out.println("Assertion is True...");
+    System.out.println("Team-C");
   }
 
 }

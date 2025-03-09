51ec00f0c95a076ab8cfabd4033b4f2d320b8161
haristr7 <harikrishnan0132@gmail.com>
2025-03-09 14:15:58 +0530

    Commited By Harikrishnan

--- src/main/java/com/FebIPT/App.java
+++ src/main/java/com/FebIPT/App.java
@@ -8,13 +8,21 @@ public class App {
 
 	public void helloWord() {
 		System.out.println("Team-B& Hari& Ajith");
+		System.out.println("                    ");
+		System.out.println("                    ");
 	}
 
 	public void helloMars() {
 		System.out.println("Harikrishnan From Nov IPT");
+		System.out.println("                    ");
 	}
 
 	public void helloJupiter() {
-		System.out.println("HariStr From Nov IPT 2024 And Edited");
+		System.out.println("HariStr From Nov IPT 2024 And Edited           ");
+		
+		
+		System.out.println("                    ");
+		
+		System.out.println("                    ");
 	}
 }
